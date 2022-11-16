bbe98ceff7e4f54b49d6de0f59a257f3ecfe758e
Gabriel018 <duke.comandos@gmail.com>
2022-11-15 21:55:42 -0300

    ,1,1

---
+++ response.js
@@ -0,0 +1,5 @@
+const Respostas = {
+    ola: "Como vai?",
+    horas: new Date().toLocaleTimeString(),
+    'Bom Dia': "Bom dia Senhor"
+}
--- script.js
+++ script.js
@@ -5,12 +5,31 @@ const enviar = document.querySelector(".enviar")
 enviar.addEventListener("click",()=> renderUserMenssager())
     const renderUserMenssager = () =>{
         const userInput = texto.value;
-        renderMenssagerEle(userInput)
+        texto.value = ''
+        renderMenssagerEle(userInput,'usuario')
+        renderChatbootReponse(userInput)
     }
-    const renderMenssagerEle = (txt) => {
+    const renderChatbootReponse = (userInput) =>{
+      
+        const res = getResponde(userInput)
+        renderMenssagerEle(res)
+    }
+
+    const renderMenssagerEle = (txt,type) => {
+        let className = "user-messager"
+        if (type !== 'usuario'){
+            className = "chatboot"
+        }
         const MenssagerEle = document.createElement("div")
         const txtNode = document.createTextNode(txt)
-        MenssagerEle.classList.add("user-messager")
+        MenssagerEle.classList.add(className)
         MenssagerEle.append(txtNode)
         ChatBoot.append(MenssagerEle)
+    }
+
+    const getResponde = (userInput) => {  
+       return Respostas[userInput] == undefined ? "Nao entendi": Respostas[userInput]
+
+       
+
     }
